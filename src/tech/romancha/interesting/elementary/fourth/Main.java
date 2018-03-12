a2383860ac36fb48a996b6f25805c339ff737d75
romcha68 <romcha68@ukr.net>
2018-03-12 15:32:38 +0200

    made corrections

--- src/tech/romancha/interesting/elementary/fourth/Main.java
+++ src/tech/romancha/interesting/elementary/fourth/Main.java
@@ -2,14 +2,18 @@ package tech.romancha.interesting.elementary.fourth;
 
 import java.util.Scanner;
 
-public class Main {
+/*
+* Ask a number n and display the sum from 1 to n
+ */
+
+public class  Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number n: ");
         int n = scanner.nextInt();
         int sum = 0;
         for(int i = 0; i <= n; i++){
-            sum = sum + i;
+            sum +=i;
         }
         System.out.println("Sum n = " + sum);
     }
