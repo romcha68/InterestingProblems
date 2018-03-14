3a4e0120cf1e1493fabcf19dd0c990be556ffc34
romcha68 <romcha68@ukr.net>
2018-03-14 15:12:58 +0200

    The solution to 7 problems

---
+++ src/tech/romancha/interesting/elementary/sevens/Main.java
@@ -0,0 +1,15 @@
+package tech.romancha.interesting.elementary.sevens;
+
+/**
+ * Multiplication table by 12.
+ */
+public class Main {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 12; i++){
+            for(int j = 1; j <=12; j++){
+                System.out.printf("%4d ",(i * j));
+            }
+            System.out.println(" ");
+        }
+    }
+}
